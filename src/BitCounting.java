c508811ed5af3e8d1b80c6e303925a1713069a5b
xndy930 <im.dengcm@gmail.com>
2018-06-29 21:15:01 +0800

    位计数

--- src/BitCounting.java
+++ src/BitCounting.java
@@ -1,10 +1,7 @@
 public class BitCounting {
-    public static void main(String[] args) {
-        int num = BitCounting.countBits(854);
-        System.out.println(num);
-
-    }
-
+    /**
+     * 编写一个以整数作为输入的函数，并返回该数字的二进制表示中等于1的位数。您可以保证输入是非负的。
+     */
     public static int countBits(int n) {
         int r = 0;        //记录余数
         int count = 0;    //1的次数
@@ -16,5 +13,11 @@ public class BitCounting {
             }
         }
         return count;
+    }
+
+    public static void main(String[] args) {
+        int num = BitCounting.countBits(854);
+        System.out.println(num);
+
     }
 }
