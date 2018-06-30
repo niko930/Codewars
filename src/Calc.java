204428f6a0cde4777adde1dce300417540816bf5
xndy930 <im.dengcm@gmail.com>
2018-06-30 21:15:11 +0800

    后缀表达式

--- src/Calc.java
+++ src/Calc.java
@@ -1,12 +1,11 @@
 import java.util.Stack;
 
 public class Calc {
-	public static void main(String[] args) {
-		String string = "5 1 2 + 4 * + 3 -";
-		double evaluate = Calc.evaluate(string);
-		System.out.println(evaluate);
-	}
-	
+	/**
+	 * 后缀表达式，又称逆波兰式，
+	 * 指的是不包含括号，运算符放在两个运算对象的后面，
+	 * 所有的计算按运算符出现的顺序，严格从左向右进行（不再考虑运算符的优先规则）。
+	 */
 	public static double evaluate(String expr) {
 		if(expr == null || "".equals(expr))
 		return 0;
@@ -32,7 +31,12 @@ public class Calc {
 				}
 			}
 		}
-		
 	    return Integer.valueOf(stack.pop());
 	  }
+
+	public static void main(String[] args) {
+		String string = "5 1 2 + 4 * + 3 -";
+		double evaluate = Calc.evaluate(string);
+		System.out.println(evaluate);
+	}
 }
