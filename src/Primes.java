719a4d7c964226d78b36e20a85a8e66fdd46752a
xndy930 <im.dengcm@gmail.com>
2018-09-15 19:15:14 +0800

    创建源源不断的素数

--- src/Primes.java
+++ src/Primes.java
@@ -11,7 +11,6 @@ public class Primes {
     public static boolean isPrime(int number) {
         for (int i = 2; i < (int) Math.sqrt(number) + 1; i++)
             if (number % i == 0) return false;
-
         return true;
     }
 }
