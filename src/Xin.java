93f5a5ce2f4c2cb63719d8930266210b14e06f7d
xndy930 <im.dengcm@gmail.com>
2018-09-30 18:33:45 +0800

    打印心形

--- src/Xin.java
+++ src/Xin.java
@@ -3,16 +3,15 @@ public class Xin {
         /**
          *打印心形
          */
-            for(float y = (float) 1.5;y>-1.5;y -=0.1)  {
-                for(float x= (float) -1.5;x<1.5;x+= 0.05){
-                    float a = x*x+y*y-1;
-                    if((a*a*a-x*x*y*y*y)<=0.0)  {
-                        System.out.print("^");
-                    }
-                    else
-                        System.out.print(" ");
-                }
-                System.out.println();
+        for (float y = (float) 1.5; y > -1.5; y -= 0.1) {
+            for (float x = (float) -1.5; x < 1.5; x += 0.05) {
+                float a = x * x + y * y - 1;
+                if ((a * a * a - x * x * y * y * y) <= 0.0) {
+                    System.out.print("^");
+                } else
+                    System.out.print(" ");
             }
+            System.out.println();
+        }
     }
 }
