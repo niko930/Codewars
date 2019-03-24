4fa440783897d43b81b9288724280d1930c4ae11
xndy930 <im.dengcm@gmail.com>
2019-03-24 20:26:11 +0800

    数组中是否存在下标,使左右两边合相等

--- src/findEvenIndex.java
+++ src/findEvenIndex.java
@@ -5,27 +5,8 @@ public class findEvenIndex {
     public static void main(String[] args) {
         int[] arr = {2824, 1774, -1490, -9084, -9696, 23094};
         int evenIndex = findEvenIndex.findEvenIndex(arr);
-        int evenIndex2 = findEvenIndex.findEvenIndex2(arr);
         System.out.println(evenIndex);
-        System.out.println(evenIndex2);
     }
-
-    public static int findEvenIndex2(int[] arr) {
-        int sumRight = Arrays.stream(arr).sum();
-        int sumLeft = 0;
-
-        for(int i=0;i<arr.length;i++){
-            sumLeft += arr[i];
-            sumRight -= arr[i];
-
-//            if(sumLeft == sumRight){
-//                return i;
-//            }
-        }
-        return -1;
-    }
-
-
 
 
     public static int findEvenIndex(int[] arr) {
